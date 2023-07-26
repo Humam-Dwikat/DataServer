ebea499b8cb890db8d0ca95a891692628bb78d3b
Humam Dwikat <humamdwik@gmail.com>
2023-07-26 21:08:23 +0300

    edit on endpoint

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import uvicorn
-from elasticsearch import Elasticsearch
 from fastapi import FastAPI
 
 from routers import tweet
 
 app = FastAPI()
-es = Elasticsearch()
 app.include_router(router=tweet.router)
-
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0')
--- routers/tweet.py
+++ routers/tweet.py
@@ -4,14 +4,14 @@ from db_client.client_elasticsearch import ClientES
 from repo.repos import Repo
 from service.tweet import TweetOperation
 
-router = APIRouter(prefix='/v1/get')
+router = APIRouter(prefix='/v1/search')
 
 es = ClientES()
 repo = Repo(es=es)
 client = TweetOperation(repo)
 
 
-@router.get('/alltweets')
+@router.get('/tweets')
 async def get_tweet():
     res = await client.get_tweet(index_name='index1')
     return res
